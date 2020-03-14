54de142f2a3dae13cfe6d3245a23484b32a0e138
大河戸裕一 <okodoyuichi@MacBook-Pro-2.local>
2020-03-15 08:11:15 +0900

    prop-types

--- udemy-react/src/App.js
+++ udemy-react/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const App = () => {
   const profiles = [
-    {
-      name: "Taro",
-      age: 10
-    },
-    {
-      name: "hanako",
-      age:9
-    },
-    {
-      name: "okodo"
-    }
+    {name: "Taro",age: 10},
+    {name: "hanako",age:9},
+    {name: "okodo"}
   ]
 
   return (
@@ -22,23 +15,17 @@ const App = () => {
           return <User name={profile.name} age={profile.age} key={index}/>
         })
       }
-      <Cat />
-      <User name={'Taro'} age={10}/>
-      <User name={'Hanako'} age={9}/>
     </div>
   )
-}
-
-const Cat = () => {
-  return <div>Meow!!</div>
 }
 
 const User = (props) => {
   return <div>Meow! {props.name}, your age is {props.age}</div>
 }
 
-User.defaultProps = {
-  age:1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
